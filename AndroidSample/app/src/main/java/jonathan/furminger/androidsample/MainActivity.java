84a764c4dd063003180f1b9c555e289a4683f2c6
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-01 10:55:20 +0000

    Android Studio sample app changed from R.id to bindings (chapter 11)

--- AndroidSample/app/src/main/java/jonathan/furminger/androidsample/MainActivity.java
+++ AndroidSample/app/src/main/java/jonathan/furminger/androidsample/MainActivity.java
@@ -6,29 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+//import android.widget.EditText;
+//import android.widget.TextView;
+import jonathan.furminger.androidsample.databinding.ActivityMainBinding;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void convertCurrency(View view) {
 
-        EditText dollarText = findViewById(R.id.dollarText);
-        TextView textView = findViewById(R.id.textView);
+//        EditText dollarText = findViewById(R.id.dollarText);
+//        TextView textView = findViewById(R.id.textView);
 
-        if(!dollarText.getText().toString().equals("")) {
+//        if(!dollarText.getText().toString().equals("")) {
+        if(!binding.dollarText.getText().toString().equals("")) {
 
-            float dollarValue = Float.parseFloat((dollarText.getText().toString()));
+//            float dollarValue = Float.parseFloat((dollarText.getText().toString()));
+            float dollarValue = Float.valueOf((binding.dollarText.getText().toString()));
+
             float euroValue = dollarValue * 0.85F;
-            textView.setText((String.format(Locale.ENGLISH, "%f", euroValue)));
+//            textView.setText((String.format(Locale.ENGLISH, "%f", euroValue)));
+            binding.textView.setText((String.format(Locale.ENGLISH, "%f", euroValue)));
+
         } else {
-            textView.setText(R.string.no_value_string);
+            binding.textView.setText(R.string.no_value_string);
+//            textView.setText(R.string.no_value_string);
         }
     }
 }
