69465576162171e153dbf3035e17abe12ae3fe42
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-02 15:05:59 +0000

    Using Fragments in Android Studio

---
+++ FragmentExample/app/src/main/java/com/ebookfrenzy/fragmentexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ebookfrenzy.fragmentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ebookfrenzy.fragmentexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
