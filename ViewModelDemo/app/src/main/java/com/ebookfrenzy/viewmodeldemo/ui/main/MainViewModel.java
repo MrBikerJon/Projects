319970b969e4dd65d36a36ee13b20a28a8f7471c
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-03 19:02:48 +0000

    Android Jetpack Lifecycle Awareness Tutorial
    Chapter 39

--- ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
+++ ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
@@ -2,9 +2,11 @@ package com.ebookfrenzy.viewmodeldemo.ui.main;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.SavedStateHandle;
 
 public class MainViewModel extends ViewModel {
 
+    private static final String RESULT_KEY = "Euro Value";
     private static final Float rate = 0.74F;
     public MutableLiveData<String> dollarValue = new MutableLiveData<>();
     public MutableLiveData<Float> result = new MutableLiveData<>();
