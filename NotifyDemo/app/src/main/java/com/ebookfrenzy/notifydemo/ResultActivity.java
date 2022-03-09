3230611cc6dd500418d85432924e25d488781e7f
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-09 17:21:21 +0000

    Android TableLayout and TableRow Tutorial - chapter 66

---
+++ NotifyDemo/app/src/main/java/com/ebookfrenzy/notifydemo/ResultActivity.java
@@ -0,0 +1,29 @@
+package com.ebookfrenzy.notifydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ebookfrenzy.notifydemo.databinding.ActivityMainBinding;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+    }
+
+}
