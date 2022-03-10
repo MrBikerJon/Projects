3bc3337d0061ad773bc2dc64461a6b18f2cf8a19
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-10 16:05:18 +0000

    Android Room Database and Repository tutorial - chapter 67

---
+++ RoomDemo/app/src/main/java/com/ebookfrenzy/roomdemo/ProductRepository.java
@@ -0,0 +1,74 @@
+package com.ebookfrenzy.roomdemo;
+
+import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ProductRepository {
+
+    private final MutableLiveData<List<Product>> searchResults =
+            new MutableLiveData<>();
+    private List<Product> results;
+    private final LiveData<List<Product>> allProducts;
+    private final ProductDao productDao;
+
+    public ProductRepository(Application application) {
+        ProductRoomDatabase db;
+        db = ProductRoomDatabase.getDatabase(application);
+        productDao = db.productDao();
+        allProducts = productDao.getAllProducts();
+    }
+
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override public void handleMessage(Message msg) {
+            searchResults.setValue(results);
+        }
+    };
+
+    public void insertProduct(Product newproduct) {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            productDao.insertProduct(newproduct);
+        });
+        executor.shutdown();
+    }
+
+    public void deleteProduct(String name) {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            productDao.deleteProduct(name);
+        });
+        executor.shutdown();
+    }
+
+    public void findProduct(String name) {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            results = productDao.findProduct(name);
+            handler.sendEmptyMessage(0);
+        });
+        executor.shutdown();
+    }
+
+    // Method that the ViewModel can call to obtain references to the allProducts live data object
+    public LiveData<List<Product>> getAllProducts() {
+        return allProducts;
+    }
+
+    // Method that the ViewModel can call to obtain references to the searchResults live data object
+    public MutableLiveData<List<Product>> getSearchResults() {
+        return searchResults;
+    }
+
+}
