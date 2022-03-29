dbd361184c39721d7f9e1703522f6da3aae2de5f
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-03-29 22:40:33 +0100

    Android 9 Deelopment cookbook - changing View properties at runtime

---
+++ RecyclerView/app/src/test/java/com/furminger/recyclerview/MyAdapterTest.java
@@ -0,0 +1,15 @@
+package com.furminger.recyclerview;
+
+import junit.framework.TestCase;
+
+public class MyAdapterTest extends TestCase {
+
+    public void testOnCreateViewHolder() {
+    }
+
+    public void testOnBindViewHolder() {
+    }
+
+    public void testGetItemCount() {
+    }
+}
