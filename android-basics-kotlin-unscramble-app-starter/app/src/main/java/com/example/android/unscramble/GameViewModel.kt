ad3dd1dfa03973a1a837c62681f81069a1d16cbe
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-04-17 21:21:57 +0100

    Android Developer Certification - ViewModel - completed but doesn't work properly

--- android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/GameViewModel.kt
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/GameViewModel.kt
@@ -6,31 +6,62 @@ import com.example.android.unscramble.ui.game.allWordsList
 
 class GameViewModel : ViewModel() {
 
-    private fun getNextWord() {
-        currentWord = allWordsList.random()
-        val tempWord = currentWord.toCharArray()
-        tempWord.shuffle()
-        while(String(tempWord).equals(currentScrambledWord, false)) {
-            tempWord.shuffle()
-        }
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
 
-    }
+    private val MAX_NO_OF_WORDS = 10
+
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private lateinit var _currentScrambledWord: String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
-
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    /*
+* Updates currentWord and currentScrambledWord with the next word.
+*/
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    /*
+* Returns true if the current word count is less than MAX_NO_OF_WORDS.
+* Updates the next word.
+*/
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 
 
